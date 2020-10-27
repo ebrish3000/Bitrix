d6d2e9c8408bb5df31b4d92aa4066d542ddc74d0
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-10-27 01:16:56 -0700

    Update Practice_Git.java

--- src/test/java/com/bitrix/stepDefinitions/Practice_Git.java
+++ src/test/java/com/bitrix/stepDefinitions/Practice_Git.java
@@ -3,5 +3,6 @@ package com.bitrix.stepDefinitions;
 public class Practice_Git {
     public static void main(String[] args) {
         System.out.println("add -> commit-> push");
+         System.out.println("add -> commit-> push-> pull");
     }
 }
